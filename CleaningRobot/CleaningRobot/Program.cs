4735e98fc3d7cfe482164151585ed77d50e6e91a
Valentin Horacek <valentin.horacek@gmail.com>
2025-03-11 20:31:47 +0100

    Project init

---
+++ CleaningRobot/CleaningRobot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CleaningRobot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
